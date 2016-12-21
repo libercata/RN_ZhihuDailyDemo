84d68fad5e74446fdbe785b32c6afb5ba6bcb6d7
imhwn@vip.qq.com <imhwn@vip.qq.com>
2016-12-21 10:22:22 +0800

    test

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class RN_ZhihuDailyDemo extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!fff
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
